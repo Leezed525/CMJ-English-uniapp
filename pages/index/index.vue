aaa02e00656832415c22f65d7c428b7724684794
leezed <975500562@qq.com>
2022-01-23 17:35:30 +0800

    初始化了uniapp项目

---
+++ pages/index/index.vue
@@ -0,0 +1,46 @@
+<template>
+	<view class="body">
+		<uni-search-bar placeholder="搜索" @confirm="confirm()"/>
+		<view class="head">
+			<view class="head-item">
+				1
+			</view>
+			<view class="head-item"> 
+				2
+			</view>
+			<view class="head-item">
+				3
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			confirm(val){
+				console.log(val)
+				console.log("confirm")
+			}
+		}
+	}
+</script>
+
+<style>
+	.head{
+		display: flex;
+		flex-direction: row;
+		height: 30%;
+	}
+	.head-item{
+		width: 100%;
+	}
+</style>
